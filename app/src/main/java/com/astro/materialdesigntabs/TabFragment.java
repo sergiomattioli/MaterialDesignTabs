c5209eeb7034e9680787ef411938224ded2df834
Sergio Mattioli <sergiomattioli@gmail.com>
2015-10-06 21:07:01 -0300

    Initial commit

---
+++ app/src/main/java/com/astro/materialdesigntabs/TabFragment.java
@@ -0,0 +1,46 @@
+package com.astro.materialdesigntabs;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+/**
+ * Created by Sergio Mattioli on 06/10/15.
+ */
+public class TabFragment extends Fragment {
+
+    private ListAdapter mAdapter;
+
+    private String mItemData = "Lorem Ipsum is simply dummy text of the printing and "
+            + "typesetting industry Lorem Ipsum has been the industry's standard dummy text ever since the 1500s";
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment, container, false);
+
+        RecyclerView recyclerView = (RecyclerView) view.findViewById(
+                R.id.fragment_list_rv);
+
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getActivity());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.setHasFixedSize(true);
+
+        String[] listItems = mItemData.split(" ");
+
+        List<String> list = new ArrayList<String>();
+        Collections.addAll(list, listItems);
+
+        mAdapter = new ListAdapter(list);
+        recyclerView.setAdapter(mAdapter);
+
+        return view;
+    }
+}
